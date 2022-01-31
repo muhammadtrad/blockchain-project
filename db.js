86a2da8f0e12f38aacd68e172032e663b63bbadb
Muhammad Trad <muhammad.trad@yahoo.com>
2022-01-30 18:52:45 -0800

    feat: adjust target difficulty

--- db.js
+++ db.js
@@ -3,6 +3,7 @@ const Blockchain = require("./models/Blockchain");
 const db = {
   blockchain: new Blockchain(),
   utxos: [],
+  averageTimeToMineBlock: 5000,
 };
 
 module.exports = db;
--- mine.js
+++ mine.js
@@ -3,8 +3,12 @@ const Transaction = require("./models/Transaction");
 const UTXO = require("./models/UTXO");
 const db = require("./db");
 const { PUBLIC_KEY } = require("./config");
-const TARGET_DIFFICULTY = BigInt("0x0" + "F".repeat(63));
+const DIFFICULTY_VALUE = 1;
+
 const BLOCK_REWARD = 10;
+
+let calculatedTargetDiffulty = () =>
+  "0x" + "0".repeat(DIFFICULTY_VALUE) + "F".repeat(63);
 
 let mining = true;
 mine();
@@ -18,6 +22,29 @@ function stopMining() {
   mining = false;
 }
 
+const increaseTargetDifficulty = () => {
+  DIFFICULTY_VALUE += 1;
+};
+
+const increaseTargetDifficulty = () => {
+  DIFFICULTY_VALUE -= 1;
+};
+
+let totalSeconds = 0;
+function setTime() {
+  totalSeconds += 250;
+  console.log("totalSeconds", totalSeconds);
+
+  if (db.blockchain.blockHeight() % 5 === 0) {
+    if (totalSeconds / 5 < 5000) {
+      increaseTargetDifficulty();
+    } else {
+      decreaseTargetDifficulty();
+    }
+    totalSeconds = 0;
+  }
+}
+
 function mine() {
   if (!mining) return;
 
@@ -29,7 +56,7 @@ function mine() {
   const coinbaseTX = new Transaction([], [coinbaseUTXO]);
   block.addTransaction(coinbaseTX);
 
-  while (BigInt("0x" + block.hash()) >= TARGET_DIFFICULTY) {
+  while (BigInt("0x" + block.hash()) >= calculatedTargetDiffulty()) {
     block.nonce++;
   }
 
@@ -42,8 +69,8 @@ function mine() {
       block.nonce
     }`
   );
-
-  setTimeout(mine, 2500);
+  setInterval(setTime, 250);
+  setTimeout(mine, 5000);
 }
 
 module.exports = {
